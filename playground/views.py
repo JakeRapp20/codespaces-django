1d5b9f545402c30c42a9e5a0a3ab60cbd23d1a7b
JakeRapp20 <119356907+JakeRapp20@users.noreply.github.com>
2023-06-21 06:34:02 +0000

    test

--- playground/views.py
+++ playground/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
-def say_hello(request):
-    return HttpResponse('Hello World')
+def var_test(request):
+    if request.method == 'POST':
+        user_input = request.POST.get('user_input', '')
+        return render(request, 'result.html', {'user_input': user_input})
+    
+    return render(request, 'input.html')
+
