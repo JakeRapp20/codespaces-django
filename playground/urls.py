72e878f53a0ef3de75c75312520ae2d09f48360e
JakeRapp20 <119356907+JakeRapp20@users.noreply.github.com>
2023-06-23 04:11:50 +0000

    last step in course

--- playground/urls.py
+++ playground/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.var_test)
+    path('hello/', views.say_hello)
 ]
--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.views.decorators.csrf import csrf_exempt
 
-def var_test(request):
-    if request.method == 'POST':
-        user_input = request.POST.get('user_input', '')
-        return render(request, 'result.html', {'user_input': user_input})
-    
-    return render(request, 'input.html')
-
+def say_hello(request):
+    return render(request, 'hello.html', { 'name': 'Jake'})
